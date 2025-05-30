4da8765e0d495562aa88b72738af088ecb722f97
KaminariSora <nonthacha.h@gmail.com>
2025-05-30 16:26:50 +0700

    finish footer

--- pixela/src/Components/home.js
+++ pixela/src/Components/home.js
@@ -2,6 +2,9 @@ import './navBar.css'
 import './body.css'
 import './footer.css'
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube, faXTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
+
 
 const home = () => {
     return (
@@ -35,7 +38,7 @@ const home = () => {
                     <div className="box" id='box-2'>
                         <img src='./Images/X.png'></img>
                         <h6>ติดตามข่าวสารได้ที่ (Twitter) หลัก Pixela</h6>
-                        <a href='#'>@PixelaProject</a>
+                        <a href='https://x.com/PixelaProject' target="_blank">@PixelaProject</a>
                     </div>
                     <div className="box" id='box-3'>
                         <div className='image-wrapper'>
@@ -55,9 +58,36 @@ const home = () => {
                 
             </div>
             <footer className='footer'>
-                <div className='footer-container'>1</div>
-                <div className='footer-container'>2</div>
-                <div className='footer-container'>3</div>
+                <div className='footer-container'>
+                    <h1 className='quicksand' id='contact'>Contact Us</h1>
+                    <h4 className='quicksand' id='name'>Pixela Official Co., Ltd.<strong></strong></h4>
+                    <h6 className='quicksand' id='location'>5 Soi Vibhavadi Rangsit 16/20 Vibhavadi Rangsit Road Ratchadaphisek Subdistrict, Din Daeng District, Bangkok 10400</h6>
+                </div>
+                <div className='footer-container'>
+                    <h4 className='quicksand' id='email'><strong>Email</strong> : info@pixela.me</h4>
+                    <h4 className='quicksand'><strong>Tel</strong> : 0xx-xxx-xxxx</h4>
+                </div>
+                <div className='footer-container'>
+                    <h1 className='quicksand' id='contact'>Meet Us At</h1>
+                    <ul>
+                        <li>
+                            <a href='https://www.youtube.com/@PixelaOfficial' target='_blank'>
+                                <FontAwesomeIcon icon={faYoutube} />: Youtube <span className='light-weight-text'>(@PixelaOfficial)</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://x.com/PixelaProject' target='_blank'>
+                                <FontAwesomeIcon icon={faXTwitter} />: X(Twitter) <span className='light-weight-text'>(@PixelaProject)</span>
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href='https://www.facebook.com/pixelaproject' target='_blank'>
+                                <FontAwesomeIcon icon={faFacebook} />: Facebook <span className='light-weight-text'>(/pixelaproject)</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </footer>
         </div>
     )
