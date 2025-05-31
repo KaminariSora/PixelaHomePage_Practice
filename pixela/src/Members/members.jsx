02ef8feb4ac151514544124acf23fcb5b7fb9136
KaminariSora <nonthacha.h@gmail.com>
2025-05-31 21:23:29 +0700

    Finish member page

--- pixela/src/Members/members.jsx
+++ pixela/src/Members/members.jsx
@@ -10,19 +10,19 @@ const Member = () => {
                     <div className="generation"><h1 className="quicksand project">Pixela Project</h1></div>
                     <div className="member">
                         <span id="hongfei">
-                            <a href="#" target="_blank"><img src="/First_generation/Hongfei.png"></img></a>
+                            <a href="https://www.youtube.com/@HinabeHongFei" target="_blank"><img src="/First_generation/Hongfei.png"></img></a>
                             <div className="member-name"><h4 className="quicksand">Hinabe Hongfei</h4></div>
                         </span>
                         <span id="juju">
-                            <a href="#" target="_blank"><img src="/First_generation/Juju.png"></img></a>
+                            <a href="https://www.youtube.com/@LagunaJuJu" target="_blank"><img src="/First_generation/Juju.png"></img></a>
                             <div className="member-name"><h4 className="quicksand">Laguna Juju</h4></div>
                         </span>
                         <span id="zelina">
-                            <a href="#" target="_blank"><img src="/First_generation/Zelina.png"></img></a>
+                            <a href="https://www.youtube.com/@PrincessZelina" target="_blank"><img src="/First_generation/Zelina.png"></img></a>
                             <div className="member-name"><h4 className="quicksand">Princess Zelina</h4></div>
                         </span>
                         <span id="melita">
-                            <a href="#" target="_blank"><img src="/First_generation/MelitaX.png"></img></a>
+                            <a href="https://www.youtube.com/@MelitaX-san" target="_blank"><img src="/First_generation/MelitaX.png"></img></a>
                             <div className="member-name"><h4 className="quicksand">Melita X</h4></div>
                         </span>
                     </div>
@@ -31,28 +31,20 @@ const Member = () => {
                     <div className="generation"><h1 className="quicksand isekai">Pixela Isekai</h1></div>
                     <div className="member">
                         <span id="estaa">
-                            <a href="#" target="_blank"><img src="/Second_generation/Estaa.png"></img></a>
+                            <a href="https://www.youtube.com/@JollyEstaa" target="_blank"><img src="/Second_generation/Estaa.png"></img></a>
                             <div className="member-name"><h4 className="quicksand">Jolly Estaa</h4></div>
                         </span>
                         <span id="elvene">
-                            <a href="#" target="_blank"><img src="/Second_generation/Elvene.png"></img></a>
+                            <a href="https://www.youtube.com/@AranisElvene" target="_blank"><img src="/Second_generation/Elvene.png"></img></a>
                             <div className="member-name"><h4 className="quicksand">Aranis Elvene</h4></div>
                         </span>
                         <span id="lafy">
-                            <a href="#" target="_blank"><img src="/Second_generation/Lafy.png"></img></a>
+                            <a href="https://www.youtube.com/@HanabiLafy" target="_blank"><img src="/Second_generation/Lafy.png"></img></a>
                             <div className="member-name"><h4 className="quicksand">Hanabi Lafy</h4></div>
                         </span>
-                        <span id="kona">
-                            <a href="#" target="_blank"><img src="/Second_generation/Kona.png"></img></a>
-                            <div className="member-name"><h4 className="quicksand">Draki Kona</h4></div>
-                        </span>
                         <span id="mewten">
-                            <a href="#" target="_blank"><img src="/Second_generation/Mewten.png"></img></a>
+                            <a href="https://www.youtube.com/@KitsunekoMewten" target="_blank"><img src="/Second_generation/Mewten.png"></img></a>
                             <div className="member-name"><h4 className="quicksand">Kisuneko Mewten</h4></div>
-                        </span>
-                        <span id="reirin">
-                            <a href="#" target="_blank"><img src="/Second_generation/Reirin.png"></img></a>
-                            <div className="member-name"><h4 className="quicksand">Kamiyu Reirin</h4></div>
                         </span>
                     </div>
                 </div>
@@ -60,8 +52,129 @@ const Member = () => {
                     <div className="generation"><h1 className="quicksand legend">Pixela Legend</h1></div>
                     <div className="member">
                         <span id="pero">
-                            <a href="#" target="_blank"><img src="/Third_generation/Pero.png"></img></a>
+                            <a href="https://www.youtube.com/@TAKOPERO" target="_blank"><img src="/Third_generation/Pero.png"></img></a>
                             <div className="member-name"><h4 className="quicksand">Superpretty TAKOPERO</h4></div>
+                        </span>
+                        <span id="arlin">
+                            <a href="https://www.youtube.com/@AkemiArlin" target="_blank"><img src="/Third_generation/Arlin.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Akemi Arlin</h4></div>
+                        </span>
+                        <span id="elze">
+                            <a href="https://www.youtube.com/@NezumiElze" target="_blank"><img src="/Third_generation/Elze.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Nezumi Elze</h4></div>
+                        </span>
+                        <span id="ciala">
+                            <a href="https://www.youtube.com/@UminoCiala" target="_blank"><img src="/Third_generation/Ciala.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Umico Ciala</h4></div>
+                        </span>
+                    </div>
+                </div>
+                <div className="section forth">
+                    <div className="generation"><h1 className="quicksand mystic">Pixela Mystic</h1></div>
+                    <div className="member">
+                        <span id="melony">
+                            <a href="https://www.youtube.com/@MycaraMelony" target="_blank"><img src="/Forth_generation/Melony.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Mycara Melony</h4></div>
+                        </span>
+                        <span id="naar">
+                            <a href="https://www.youtube.com/@NocturnazNaar" target="_blank"><img src="/Forth_generation/Naar.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Nocturnaz Naar</h4></div>
+                        </span>
+                    </div>
+                </div>
+                <div className="section solo">
+                    <div className="generation"><h1 className="quicksand sololist">Pixela Sololist</h1></div>
+                    <div className="member">
+                        <span id="roselia">
+                            <a href="https://www.youtube.com/@RoseliadeMagentia" target="_blank"><img src="/Solo_generation/Roselia.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Roselia De Magentia</h4></div>
+                        </span>
+                        <span id="hydrangea">
+                            <a href="https://www.youtube.com/@SisiraHydrangea" target="_blank"><img src="/Solo_generation/Hydrangea.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Sisira Hydrangea</h4></div>
+                        </span>
+                        <span id="divana">
+                            <a href="https://www.youtube.com/@DivinaCh.PixelaS" target="_blank"><img src="/Solo_generation/Divina.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Divina</h4></div>
+                        </span>
+                    </div>
+                </div>
+                <div className="section destiny">
+                    <div className="generation"><h1 className="quicksand pixela-destiny">Pixela Destiny</h1></div>
+                    <div className="member">
+                        <span id="grimmus">
+                            <a href="" target="_blank"><img src="/Destiny/Grimmus.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Grimus Grimm</h4></div>
+                        </span>
+                        <span id="shuun">
+                            <a href="" target="_blank"><img src="/Destiny/Shuun.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Aruna Shuun</h4></div>
+                        </span>
+                        <span id="zéphyr">
+                            <a href="" target="_blank"><img src="/Destiny/Zéphyr.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Euthalia Zéphyr</h4></div>
+                        </span>
+                        <span id="blythe">
+                            <a href="" target="_blank"><img src="/Destiny/Blythe.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Biscuit Blythe</h4></div>
+                        </span>
+                    </div>
+                </div>
+                <div className="section fifth">
+                    <div className="generation"><h1 className="quicksand worldend">Pixela World End</h1></div>
+                    <div className="member">                        <span id="mildr">
+                            <a href="https://www.youtube.com/@MildRWorldEnd" target="_blank"><img src="/Worldend/MildR.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Mild-R</h4></div>
+                        </span>
+                        <span id="debirun">
+                            <a href="https://www.youtube.com/@DebirunWorldEnd" target="_blank"><img src="/Worldend/Debirun.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Debirun</h4></div>
+                        </span>
+                    </div>
+                </div>
+                <div className="section lumina-first-myth">
+                    <div className="generation"><h1 className="quicksand first-myth">Lumina First Myth</h1></div>
+                    <div className="member">
+                        <span id="kona">
+                            <a href="https://www.youtube.com/@DrakiKona" target="_blank"><img src="/Second_generation/Kona.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Draki Kona</h4></div>
+                        </span> 
+                        <span id="reirin">
+                            <a href="https://www.youtube.com/@KamiyuReirin" target="_blank"><img src="/Second_generation/Reirin.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Kamiyu Reirin</h4></div>
+                        </span>
+                        <span id="mikael">
+                            <a href="https://www.youtube.com/@CerafineMikael" target="_blank"><img src="/Forth_generation/Mikael.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Cerafine Mikael</h4></div>
+                        </span>
+                        <span id="lilibelle">
+                            <a href="https://www.youtube.com/@ArdalitaLilibelle" target="_blank"><img src="/Forth_generation/Lilibelle.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Ardalita Lilibelle</h4></div>
+                        </span>
+                        <span id="sireen">
+                            <a href="https://www.youtube.com/@AtlanteiaSireen" target="_blank"><img src="/Forth_generation/Sireen.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Atlanteia Sireen</h4></div>
+                        </span>
+                    </div>
+                </div>
+                <div className="section lumina-world-end">
+                    <div className="generation"><h1 className="quicksand world-end">Lumina World End</h1></div>
+                    <div className="member">
+                        <span id="ami">
+                            <a href="https://www.youtube.com/@AMIWorldEnd" target="_blank"><img src="/Worldend/Ami.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Beta AMI</h4></div>
+                        </span>
+                        <span id="xonebu">
+                            <a href="https://www.youtube.com/@XonebuWorldEnd" target="_blank"><img src="/Worldend/Xonebu.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Xonebu X’thulhu</h4></div>
+                        </span>
+                        <span id="tsururu">
+                            <a href="https://www.youtube.com/@TsururuWorldEnd" target="_blank"><img src="/Worldend/Tsururu.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">Kumoku Tsururu</h4></div>
+                        </span>
+                        <span id="ashyra">
+                            <a href="https://www.youtube.com/@AshyraWorldEnd" target="_blank"><img src="/Worldend/Ashyra.png"></img></a>
+                            <div className="member-name"><h4 className="quicksand">T-Reina Ashyra</h4></div>
                         </span>
                     </div>
                 </div>
